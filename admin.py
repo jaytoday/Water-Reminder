35b328a7b3db1cbcc6b95e8d223471c696d1559b
james <james@james-desktop.(none)>
2010-02-20 23:48:06 -0800

    pork chop sandwiches

--- admin.py
+++ admin.py
@@ -11,12 +11,16 @@ import datetime
 from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.api import images
+import google.appengine.api.images
 from google.appengine.ext.webapp import template
 from google.appengine.api import users
 
 from models import Image
 
+def imageQuery():
+  return Image.all().order("date")
+  
+  
 class Admin(webapp.RequestHandler):
     """
     Admin view for the application.
@@ -25,7 +29,7 @@ class Admin(webapp.RequestHandler):
     def get(self):
         "Responds to GET requets with the admin interface"
         # query the datastore for images
-        images = Image.all().order("date").fetch(1000)
+        images = imageQuery().fetch(1000)
 
         # we are enforcing loggins so we know we have a user
         user = users.get_current_user()
@@ -35,7 +39,7 @@ class Admin(webapp.RequestHandler):
         # prepare the context for the template
         context = {
             "images": images,
-            "image_count": range(len(images)),
+            "image_count": range(len(images) + 1)[1:],
             "logout": logout,
             "site_name": "Awkward Beauty"
         }
@@ -61,56 +65,62 @@ class Deleter(webapp.RequestHandler):
 class Uploader(webapp.RequestHandler):
     "Deals with uploading new images to the datastore"
     def post(self):
-        "Upload via a multitype POST message"
-        if self.request.get('img'):
-			try:
-				# check we have numerical width and height values
-				width = int(self.request.get("width"))
-				height = int(self.request.get("height"))
-			except ValueError:
-				# if we don't have valid width and height values
-				# then just use the original image
-				image_content = images.resize(self.request.get("img"), 
-				IMAGE_WIDTH, IMAGE_HEIGHT)
-			else:
-			    # if we have valid width and height values
-			    # then resize according to those values
-			    image_content = images.resize(self.request.get("img"), width, height)
-
-			# always generate a thumbnail for use on the admin page
-			thumb_content = images.resize(self.request.get("img"), 100, 100)
+      "Upload via a multitype POST message"
+      if self.request.get('img'):
+        try:
+          # check we have numerical width and height values
+          width = int(self.request.get("width"))
+          height = int(self.request.get("height"))
+        except ValueError:
+          # if we don't have valid width and height values
+          # then just use the original image
+          image_content = google.appengine.api.images.resize(self.request.get("img"), 
+          IMAGE_WIDTH, IMAGE_HEIGHT)
+          thumb_content = google.appengine.api.images.resize(self.request.get("img"), 100, 100)
         else:
-			if not self.request.get('key'):
-			  logging.critical('No key and no image! Cannot save image.')
-			  return self.redirect('/admin')
-			  image_content = None
+            # if we have valid width and height values
+            # then resize according to those values
+            image_content = google.appengine.api.images.resize(self.request.get("img"), width, height)
+            # always generate a thumbnail for use on the admin page
+            thumb_content = google.appengine.api.images.resize(self.request.get("img"), 100, 100)
+      else:
+        image_content = None
+        if not self.request.get('key'):
+          logging.critical('No key and no image! Cannot save image.')
+          return self.redirect('/admin')
 
-        # check if image is being edited
-        if self.request.get('key'):
-          image = db.get(self.request.get("key"))
-          if self.request.get('position'):
-	          position = int(self.request.get('position'))
-	          images = Image.all().order('date').fetch(100)
-	          #image_keys = [i.key() for i in images ]
-	          import datetime
-	                
-        else:
-	      # create the image object
-          image = Image()
-          image.user = users.get_current_user()
-          image.title = self.request.get('title')
-        if image_content:
-		    # and set the properties to the relevant values
-	        image.image = db.Blob(image_content)
-	        # we always store the original here in case of errors
-	        # although it's currently not exposed via the frontend
-	        image.thumb = db.Blob(thumb_content) 
+      # check if image is being edited
+      if self.request.get('key'):
+        image = db.get(self.request.get("key"))
+        if self.request.get('position'):
+          import datetime
+          position = int(self.request.get('position'))
+          images = imageQuery().fetch(100)
+          offset_image = images.pop(position- 1)
+          if position == 1: 
+            time_offset = -datetime.timedelta(milliseconds=10)
+          else:
+            time_offset = datetime.timedelta(milliseconds=10)
+          if not offset_image.key() == image.key(): 
+            image.date = offset_image.date + time_offset
                 
-        # store the image in the datasore
-        image.put()
-        # and redirect back to the admin page
-        self.redirect('/admin')
-                
+      else:
+      # create the image object
+        image = Image()
+        image.user = users.get_current_user()
+      image.title = self.request.get('title')
+      if image_content:
+      # and set the properties to the relevant values
+        image.image = db.Blob(image_content)
+        # we always store the original here in case of errors
+        # although it's currently not exposed via the frontend
+        image.thumb = db.Blob(thumb_content) 
+              
+      # store the image in the datasore
+      image.put()
+      # and redirect back to the admin page
+      self.redirect('/admin')
+              
 # wire up the views
 application = webapp.WSGIApplication([
     ('/admin', Admin),
